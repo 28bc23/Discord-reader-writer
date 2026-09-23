3b1665a947d598e72b8cec3369b9a21739cab467
28bc23 <28bc23@proton.me>
2026-09-23 22:55:58 +0200

    silence curl; remove debug echo (empty)

--- gitsketa.sh
+++ gitsketa.sh
@@ -13,16 +13,14 @@ then
 			echo "argument 2 must be inteager greater then 0"
 			exit 2
 		fi
-	else
-		echo "empty"
 	fi
 
-	curl -H "Authorization: Bot $BOt" -X GET "https://discord.com/api/v10/channels/$CHANNELID/messages?limit=$LIMIT" | jq -r  '.[] | "\(.author.username): \(.content)"'
+	curl -s -H "Authorization: Bot $BOt" -X GET "https://discord.com/api/v10/channels/$CHANNELID/messages?limit=$LIMIT" | jq -r  '.[] | "\(.author.username): \(.content)"'
 
 elif [ "-p" == "${ARGS[0]}" ]
 then
     MSG="${ARGS[*]:1}"
-    curl -H "Authorization: Bot $BOt" -H "Content-Type: application/json" \
+    curl -s -H "Authorization: Bot $BOt" -H "Content-Type: application/json" \
         -X POST -d "$(jq -n --arg c "$MSG" '{content:$c}')" \
         "https://discord.com/api/v10/channels/$CHANNELID/messages"
 else
