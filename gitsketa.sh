d725d2fe278740cca4120c013c2859210ca0255b
28bc23 <28bc23@proton.me>
2026-09-23 22:52:50 +0200

    check if 2nd parameter is number greater than zero and add default limit of messages to 1

--- gitsketa.sh
+++ gitsketa.sh
@@ -5,7 +5,19 @@ ARGS=("$@")
 
 if [ "-r" == "${ARGS[0]}" ]
 then
-        curl -H "Authorization: Bot $BOt" -X GET "https://discord.com/api/v10/channels/$CHANNELID/messages?limit=${ARGS[1]}" | jq -r  '.[] | "\(.author.username): \(.content)"'
+	LIMIT=1
+	if [ -n "${ARGS[1]}" ]; then
+		if [[ ${ARGS[1]} =~ ^[0-9]+$ ]] && [ ${ARGS[1]} -gt 0 ]; then
+			LIMIT=${ARGS[1]}
+		else
+			echo "argument 2 must be inteager greater then 0"
+			exit 2
+		fi
+	else
+		echo "empty"
+	fi
+
+	curl -H "Authorization: Bot $BOt" -X GET "https://discord.com/api/v10/channels/$CHANNELID/messages?limit=$LIMIT" | jq -r  '.[] | "\(.author.username): \(.content)"'
 
 elif [ "-p" == "${ARGS[0]}" ]
 then
@@ -15,4 +27,5 @@ then
         "https://discord.com/api/v10/channels/$CHANNELID/messages"
 else
         echo "Z KURVY SYNU HLOUPEJ -p NEBO -r"
+	exit 2
 fi
